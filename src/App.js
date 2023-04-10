c66d9e9a7abebeac7e11343206db30042b525ec5
nwxanin <120450273+nwxanin@users.noreply.github.com>
2023-04-10 09:39:47 +0430

    footer and link to git hub added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This project is{" "}
+        <a
+          href="https://github.com/nwxanin/weather-react-w5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-Source
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
