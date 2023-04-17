d17e61ce484f800ec49f00774c26615e46a5bd83
nwxanin <120450273+nwxanin@users.noreply.github.com>
2023-04-17 11:00:46 +0430

    at the middle way of  api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,24 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let latitude = 52;
+  let longitude = 29;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=ac209dae1f283fb332a5bb7f50b0f468`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast mt-4">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
           <WeatherIcon code="02d" size={36} />
-          <div classNAme="WeatherForecast-temperature">
+          <div className="WeatherForecast-temperature">
             <span className="WeatherForecast-temperature-max">19°</span>{" "}
             <span className="WeatherForecast-temperature-min">10°</span>
           </div>
